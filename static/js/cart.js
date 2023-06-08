80714078b173c4e6cc6e6f4f2b81da86e539c11f
Fobbie <vandriadis@gmail.com>
2023-06-08 17:51:35 +0300

    Remover misstakes

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,55 +1,39 @@
-var updateBtns = document.getElementsByClassName('update-cart');
-var increaseButtons = document.getElementsByClassName('increase-item');
-var decreaseButtons = document.getElementsByClassName('decrease-item');
+var updateBtns = document.getElementsByClassName('update-cart')
 
-for (var i = 0; i < increaseButtons.length; i++) {
-  increaseButtons[i].addEventListener('click', function() {
-    var productId = this.dataset.product;
-    var action = 'add';
-    updateUserOrder(productId, action);
-  });
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
+
+		if (user == 'AnonymousUser'){
+			console.log('User is not authenticated')
+			
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
 
-for (var i = 0; i < decreaseButtons.length; i++) {
-  decreaseButtons[i].addEventListener('click', function() {
-    var productId = this.dataset.product;
-    var action = 'remove';
-    updateUserOrder(productId, action);
-  });
-}
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
 
-for (var i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.productId;
-        var action = this.dataset.action;
-        console.log('productId:', productId, 'Action:', action);
+		var url = '/update_item/'
 
-        console.log('USER:', user);
-        if (user == 'AnanymousUser') {
-            console.log('User is not authenticated');
-        } else {
-            updateUserOrder(productId, action);
-        }
-    });
-}
-
-// console.log(csrftoken)
-function updateUserOrder(productId, action) {
-    var url = "/update_item/";
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken,
-        },
-        body: JSON.stringify({ productId: productId, action: action }),
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log("Data:", data);
-            location.reload();
-        });
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
 }
 
